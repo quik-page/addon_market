24276b9f3fe1acd27a2461874568a50d07a62211
siquan001 <chensiquan2022@163.com>
2024-07-10 09:49:58 +0800

    test

--- 000/index.js
+++ 000/index.js
@@ -1,3 +1,3 @@
-/*QUIK_ADDON 1|Test|1|1.0|For Test|siquan||https://siquan001.github.io/|version.txt|siquan.test */
+/*QUIK_ADDON 1|Test|3|3.0|For Test|siquan||https://siquan001.github.io/|version.txt|siquan.test */
 
-quik.alert("测试插件弹窗")
+quik.alert("测试插件弹窗3")
